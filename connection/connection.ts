0d33d7006359a7428de6cb9f928fa23dad768c13
Hubert Lin <linhub15@gmail.com>
2022-09-21 22:51:25 -0600

    feat: auto-reconnect on `ConnectionError`

--- connection/connection.ts
+++ connection/connection.ts
@@ -953,6 +953,7 @@ export class Connection {
     } catch (e) {
       if (e instanceof ConnectionError) {
         await this.end();
+        return await this.query(query);
       }
       throw e;
     } finally {
